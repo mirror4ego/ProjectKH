5afd9f9a89babc00e476d50840168b3aa7e7cc18
mirror4ego <mirror4ego@naver.com>
2017-05-10 14:33:12 +0900

    뷰 클래스 재구성

--- src/projectKH/Test.java
+++ src/projectKH/Test.java
@@ -8,7 +8,10 @@ public class Test {
 		CustomerDao customerDao = new DaoFactory().customerDao();
 		OrderInfoDao orderInfoDao = new DaoFactory().orderInfoDao();
 		
-		OrderView_sub ex = new OrderView_sub(null); 
+		//메인뷰 생성 테스트
+		MainView ex = new MainView("매장관리"); 
+		System.out.println("메인 뷰 생성 완료");
+		
 		Customer customer01 = new Customer(8, "19990904",	"01023303333", "서울", "금천구", "금천동", "162-33 9층 202호", 2, 44);
 		Customer customer02 = new Customer(2, "20770913",	"01088482339", "충남", "공주시", "공주동", "45-33 7층 404호", 8, 23);
 		Customer customer03 = new Customer(3, "20180814",	"01018239583", "서울", "동작구", "사당동", "45-83 001호", 3, 99);
@@ -17,11 +20,7 @@ public class Test {
 		OrderInfo orderInfo01 = new OrderInfo(7, "20170501", "1", "1", 2, 10, "없음", 3, "20180303", "1", "1", "1", "1", "20170501");
 		OrderInfo orderInfo02 = new OrderInfo(2, "20170501", "0", "1", 2, 10, "빨리 배달", 3, "20170403", "1", "0", "1", "1", "20170523");
 		OrderInfo orderInfo03 = new OrderInfo(3, "20170430", "0", "1", 2, 2, "오늘 10시까지 배달", 3, "20170403", "1", "1", "1", "1", "20170403");
-		OrderInfo orderInfo04 = new OrderInfo(4, "20170420", "1", "1", 2, 9, "오는 길에 콜라", 3, "20170403", "1", "1", "0", "0", "20170502");
-		
-		//MainView 테스트
-		//MainView mainView = new MainView("매장 관리");
-		//System.out.println("메인뷰 생성 완료");		
+		OrderInfo orderInfo04 = new OrderInfo(4, "20170420", "1", "1", 2, 9, "오는 길에 콜라", 3, "20170403", "1", "1", "0", "0", "20170502");		
 		
 		//테이블 데이터 삭제 테스트
 		customerDao.deleteAll();
