866fb618150a702f3167c40dde4c09ffdb14145f
mirror4ego <mirror4ego@naver.com>
2017-05-15 12:48:29 +0900

    Mainview에서 데이터 분석 클릭시 다빈 작업하던 프로토 타입 볼 수 있도록 해둠

--- src/view/UserListView.java
+++ src/view/UserListView.java
@@ -1,10 +1,13 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.SQLException;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -27,13 +30,13 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 	JScrollPane jScrollPane1 = new JScrollPane(jTable1);
 	JPanel jPanel1 = new JPanel();
 	JButton jButton1 = new JButton("회원가입");
-	MemberDAO dao = new MemberDAO();
+	UserInfoDao dao = new UserInfoDao();
 	UserInfoDao userInfoDao = new UserInfoDao();
 	
-	public UserListView(){
-		super("사용자 목록");
-
-		vector1 = userInfoDao.getMemberList();
+	public UserListView() throws ClassNotFoundException, SQLException{
+		super("사용자 관리");
+		jTableRefresh();
+		vector1 = userInfoDao.getUserList();
 		System.out.println("v=" + vector1);
 		vector2 = getColumn();
 
@@ -45,28 +48,30 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 
 		jTable1.addMouseListener(this); //리스너 등록
 		jButton1.addActionListener(this); //회원가입버튼 리스너 등록
-
 		setSize(600,200);
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension dimension1 = toolkit.getScreenSize();
+		Dimension dimension2 = this.getSize();
+		this.setLocation((int)(dimension1.getWidth() / 2 - dimension2.getWidth() / 2), 
+				(int)(dimension1.getHeight() / 2 - dimension2.getHeight() / 2));
+
 		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
 	public Vector getColumn(){
 		vector3.add("아이디");
 		vector3.add("비밀번호");
 		vector3.add("이름");
-		vector3.add("전화");
+		vector3.add("번호");
 		vector3.add("주소");
-		vector3.add("생일");
-		vector3.add("직업");
-		vector3.add("성별");
+		vector3.add("전화번호");
 		vector3.add("이메일");
-		vector3.add("자기소개");
 		return vector3;
 	}
 
-	public void jTableRefresh(){
-		DefaultTableModel model= new DefaultTableModel(dao.getMemberList(), getColumn());
+	public void jTableRefresh() throws ClassNotFoundException, SQLException{
+		DefaultTableModel model= new DefaultTableModel(dao.getUserList(), getColumn());
 		jTable1.setModel(model);
 	}
 
@@ -74,13 +79,21 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 	public void mouseClicked(MouseEvent e) {
 		int r = jTable1.getSelectedRow();
 		String id = (String) jTable1.getValueAt(r, 0);
-		MemberProc mem = new MemberProc(id,this); //아이디를 매개변수로 수정창 생성
+		try {
+			UserRegView mem = new UserRegView(id, this);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == jButton1){
-			new MemberProc(this);
+			new UserRegView(this);
 		}
 	}
 
