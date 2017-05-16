98ddb034e4208b5605ba52a167fdb727b570d854
mirror4ego <mirror4ego@naver.com>
2017-05-16 14:15:19 +0900

    테스트 클래스 수정

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,37 +3,35 @@ package test;
 import java.io.IOException;
 import java.sql.SQLException;
 
-<<<<<<< HEAD
 import view.CustomerSearchView;
 import view.MainView;
-=======
 import service.CRM_MainLayout;
 import view.LoginView;
->>>>>>> origin/master
+
 
 
 public class Test {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-<<<<<<< HEAD
+
 		//CustomerSearchView customerSearchView = new CustomerSearchView();
 		//LoginView loginView = new LoginView();
 		MainView mainView = new MainView();
-=======
+
 
 		//LoginView loginView = new LoginView();
 		//MainView mainView = new MainView();
->>>>>>> origin/master
+
 		//UserInfoDao userInfoDao = new UserInfoDao();
 		//userInfoDao.selectAll();
 		//Login login = new Login();
 		//boolean test = login.login("mirror4ego", "1234");
-<<<<<<< HEAD
+
 		//UserSelectView userSelectView = new UserSelectView();
 		
 		
-=======
+
 		CRM_MainLayout crm = new CRM_MainLayout();
->>>>>>> origin/master
+
 	}
 }
