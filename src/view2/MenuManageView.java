5c5673097a7470226bd39d5bd6ead8f8d1e13af7
AcademyJY <mirror4ego@naver.com>
2017-05-22 21:30:10 +0900

    커밋

--- src/view2/MenuManageView.java
+++ src/view2/MenuManageView.java
@@ -9,6 +9,11 @@ import java.util.Date;
 import javax.swing.JFrame;
 
 import dao.CustomerDao;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
 
 public class MenuManageView extends JFrame{
 	public MenuManageView() throws ClassNotFoundException, SQLException {
@@ -22,6 +27,40 @@ public class MenuManageView extends JFrame{
 				(int)(dimension1.getHeight() / 2 - dimension2.getHeight() / 2));
 		this.setResizable(false);
 		this.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.ORANGE);
+		panel.setBounds(0, 0, 1150, 55);
+		getContentPane().add(panel);
+		
+		JLabel label = new JLabel("상품관리");
+		label.setForeground(Color.BLACK);
+		label.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		label.setBounds(88, 6, 133, 20);
+		panel.add(label);
+		
+		JLabel label_1 = new JLabel("상품을 등록하는 곳입니다");
+		label_1.setForeground(Color.DARK_GRAY);
+		label_1.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		label_1.setBounds(88, 28, 297, 20);
+		panel.add(label_1);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 65, 371, 617);
+		getContentPane().add(tabbedPane);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setBounds(393, 65, 589, 617);
+		getContentPane().add(tabbedPane_1);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane_1.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(null);
 		this.setVisible(true);
 	}
 }
