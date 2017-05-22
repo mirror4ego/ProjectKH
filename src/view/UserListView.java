3587da387be7e5f6d53bb92dd51f7363dae0773e
SH <hgdsmith90@gmail.com>
2017-05-22 17:26:12 +0900

    주소 구현 (구까지)

--- src/view/UserListView.java
+++ src/view/UserListView.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.SQLException;
@@ -26,7 +28,7 @@ import setting.SetLookAndFeel;
 import setting.SetUiFont;
 
 
-public class UserListView extends JFrame implements MouseListener,ActionListener{
+public class UserListView extends JFrame implements MouseListener,ItemListener{
 	//아래부터 임시 실행을 위한 메인 선언
 	public static void main(String ar[]) throws ClassNotFoundException, SQLException{
 		UserListView a = new UserListView(); //클래스 파일의 객체를 직접 생성 (실행을 위해)
@@ -64,6 +66,7 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 	private final JButton btnNewButton_5 = new JButton("삭제");
 	private final JButton btnNewButton_6 = new JButton("저장");
 	private final JButton btnNewButton_7 = new JButton("닫기");
+	
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
@@ -124,40 +127,6 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"시/도","서울","부산","대구","인천","광주","대전","울산","세종",
 				"경기","강원","충북","충남","전북","전남","경북","경남","제주"}));
 		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군"}));
-		/*(서울)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강남구","강동구","강북구","강서구","관악구","광진구","구로구",
-		 "금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구",
-		 "종로구","중구","중랑구"}));
-		(부산)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강서구","금정구","기장군","남구","동구","동래구","부산진구",
-		"북구","사상구","사하구","서구","수영구","연제구","영도구","중구","해운대구"}));
-		(대구)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","남구","달서구","달성군","동구","북구","서구","수성구","중구"}));
-		(인천)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강화군","계양구","남구","남동구","동구","부평구","서구",
-		"연수구","옹진군","중구"}));
-		(광주)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","광산구","남구","동구","북구","서구"}));
-		(대전)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","대덕구","동구","서구","유성구","중구"}));
-		(울산)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","남구","동구","북구","울주군","중구"}));
-		(세종)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군"}));
-		(경기)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","가평군","고양시 덕양구","고양시 일산동구","고양시 일산서구",
-		"과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시 분당구","성남시 수정구","성남시 중원구","수원시 권선구",
-		"수원시 영통구","수원시 장안구","수원시 팔달구","시흥시","안산시 단원구","안산시 상록구","안성시","안양시 동안구","안양시 만안구","양주시","양평군","여주시",
-		"연천군","오산시","용인시 기흥구","용인시 수지구","용인시 처인구","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"}));
-		(강원)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강릉시","고성군","동해시","삼척시","속초시","양구군",
-		"양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군","홍천군","화천군","횡성군"}));
-		(충북)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","괴산군","단양군","보은군","영동군","옥천군","음성군",
-		"제천시","증평군","진천군","청주시 상당구","청주시 서원구","청주시 청원구","청주시 흥덕구","충주시"}));
-		(충남)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","계룡시","공주시","금산군","논산시","당진시","보령시",
-		"부여군","서산시","서천군","아산시","예산군","천안시 동남구","천안시 서북구","청양군","태안군","홍성군"}));
-		(전북)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","고창군","군산시","김제시","남원시","무주군","부안군",
-		"순창군","완주군","익산시","임실군","장수군","전주시 덕진구","전주시 완산구","정읍시","진안군"}));
-		(전남)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강진군","고흥군","곡성군","광양시","구례군","나주시",
-		"담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"}));
-		(경북)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","경산시","경주시","고령군","구미시","군위군","김천시",
-		"문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시","예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군",
-		"포항시 남구","포항시 북구"}));
-		(경남)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","거제시","거창군","고성군","김해시","남해군","밀양시",
-		"사천시","산청군","양산시","의령군","진주시","창녕군","창원시 마산합포구","창원시 마산회원구","창원시 성산구","창원시 의창구","창원시 진해구","통영시",
-		"하동군","함안군","함양군","합천군"}));
-		(제주)comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","서귀포시","제주시"}));*/
-		
 		
 		JButton btnNewButton_1 =new JButton("엑셀로저장");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -233,7 +202,7 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 		textField_5.setColumns(10);
 		
 		JButton btnNewButton_8 = new JButton("X");
-		btnNewButton_8.setBounds(318, 194, 36, 23);
+		btnNewButton_8.setBounds(318, 194, 46, 23);
 		panel_1.add(btnNewButton_8);
 		list.setBounds(104, 222, 250, 108);
 		
@@ -293,8 +262,12 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 		btnNewButton_6.setBounds(1058, 611, 97, 48);
 		
 		getContentPane().add(btnNewButton_6);
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+			}
+		});
 		btnNewButton_7.setBounds(1155, 611, 97, 48);
-		
 		getContentPane().add(btnNewButton_7);
 		
 		
@@ -307,8 +280,17 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		start();
+	}
+	
+	void start(){
+		comboBox_3.addItemListener(this);
+		
 	}
 
+	
+	
+	
 	public Vector getColumn(){
 		vector3.add("아이디");
 		vector3.add("비밀번호");
@@ -327,6 +309,7 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+			
 /*		int r = jTable1.getSelectedRow();
 		String id = (String) jTable1.getValueAt(r, 0);
 		try {
@@ -340,10 +323,95 @@ public class UserListView extends JFrame implements MouseListener,ActionListener
 		}*/
 	}
 
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getStateChange()==ItemEvent.SELECTED){
+			String a = comboBox_3.getSelectedItem().toString();
+			System.out.println(a);
+
+			if(a.equals("서울")){
+				 comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강남구","강동구","강북구","강서구","관악구","광진구","구로구",
+						 "금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구",
+						 "종로구","중구","중랑구"}));
+			}
+			
+			if(a.equals("부산")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강서구","금정구","기장군","남구","동구","동래구","부산진구",
+						"북구","사상구","사하구","서구","수영구","연제구","영도구","중구","해운대구"}));
+			}
+			if(a.equals("대구")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","남구","달서구","달성군","동구","북구","서구","수성구","중구"}));
+			}
+			if(a.equals("인천")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강화군","계양구","남구","남동구","동구","부평구","서구",
+						"연수구","옹진군","중구"}));
+			}
+			if(a.equals("광주")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","광산구","남구","동구","북구","서구"}));
+			}
+			if(a.equals("대전")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","대덕구","동구","서구","유성구","중구"}));
+			}
+			if(a.equals("울산")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","남구","동구","북구","울주군","중구"}));
+			}
+			if(a.equals("세종")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군"}));
+			}
+			if(a.equals("경기")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","가평군","고양시 덕양구","고양시 일산동구","고양시 일산서구",
+						"과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시","부천시","성남시 분당구","성남시 수정구","성남시 중원구","수원시 권선구",
+						"수원시 영통구","수원시 장안구","수원시 팔달구","시흥시","안산시 단원구","안산시 상록구","안성시","안양시 동안구","안양시 만안구","양주시","양평군","여주시",
+						"연천군","오산시","용인시 기흥구","용인시 수지구","용인시 처인구","의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"}));
+			}
+			if(a.equals("강원")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강릉시","고성군","동해시","삼척시","속초시","양구군",
+						"양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군","홍천군","화천군","횡성군"}));
+			}
+			
+			if(a.equals("충북")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","괴산군","단양군","보은군","영동군","옥천군","음성군",
+						"제천시","증평군","진천군","청주시 상당구","청주시 서원구","청주시 청원구","청주시 흥덕구","충주시"}));
+				
+			}
+			if(a.equals("충남")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","계룡시","공주시","금산군","논산시","당진시","보령시",
+						"부여군","서산시","서천군","아산시","예산군","천안시 동남구","천안시 서북구","청양군","태안군","홍성군"}));
+			
+			}
+			if(a.equals("전북")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","고창군","군산시","김제시","남원시","무주군","부안군",
+						"순창군","완주군","익산시","임실군","장수군","전주시 덕진구","전주시 완산구","정읍시","진안군"}));
+					
+			}
+			if(a.equals("전남")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","강진군","고흥군","곡성군","광양시","구례군","나주시",
+						"담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군","완도군","장성군","장흥군","진도군","함평군","해남군","화순군"}));
+					
+			}
+			if(a.equals("경북")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","경산시","경주시","고령군","구미시","군위군","김천시",
+						"문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시","예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군",
+						"포항시 남구","포항시 북구"}));
+			}
+			if(a.equals("경남")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","거제시","거창군","고성군","김해시","남해군","밀양시",
+						"사천시","산청군","양산시","의령군","진주시","창녕군","창원시 마산합포구","창원시 마산회원구","창원시 성산구","창원시 의창구","창원시 진해구","통영시",
+						"하동군","함안군","함양군","합천군"}));
+						 
+			}
+			if(a.equals("제주")){
+				comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"구/군","서귀포시","제주시"}));	 
+			}
+			
+					
+		}
 	}
 
+	
+	
 	@Override
 	public void mouseEntered(MouseEvent e) {}
 	@Override
